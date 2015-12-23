8494c4c5ee4ff389b72727e50b18db81823625e1
Irvin Loza <irvinloza@gmail.com>
2015-12-23 12:04:15 -0600

    Modified gemfile and initializers for facebook auth

--- Gemfile
+++ Gemfile
@@ -15,7 +15,7 @@ gem 'bootstrap-sass', '~> 3.3.6'
 gem 'will_paginate', '3.0.7'
 gem 'bootstrap-will_paginate', '0.0.10'
 gem 'omniauth-twitter'
-
+gem 'omniauth-facebook'
 
 
 group :development, :test do
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, ENV["TWITTER_KEY"], ENV["TWITTER_SECRET"]
+  provider :facebook, ENV["FACEBOOK_KEY"], ENV["FACEBOOK_SECRET"],
+  	scope: 'public_profile,email,user_location', secure_image_url: true, info_fields: 'id,name,email,location,picture'
 end
