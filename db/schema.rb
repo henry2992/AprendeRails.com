2c593cce6629ed8291a034928880482d5ecfe912
Irvin Loza <irvinloza@gmail.com>
2015-12-23 13:36:11 -0600

    Fixed bugs and installed gems

--- db/schema.rb
+++ db/schema.rb
@@ -24,12 +24,6 @@ ActiveRecord::Schema.define(version: 20151223173158) do
     t.string   "preview_url"
   end
 
-  create_table "uploads", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "provider"
     t.string   "uid"
