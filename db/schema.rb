76f722d545da5eef9caef08c56de91c466603cfd
Irvin Loza <irvinloza@gmail.com>
2015-12-27 22:01:16 -0600

    Rake migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151225141426) do
+ActiveRecord::Schema.define(version: 20151228034847) do
 
   create_table "chapters", force: :cascade do |t|
     t.string   "title"
@@ -47,6 +47,16 @@ ActiveRecord::Schema.define(version: 20151225141426) do
     t.string   "secondary_title"
   end
 
+  create_table "identities", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.string   "provider"
+    t.string   "uid"
+  end
+
+  add_index "identities", ["user_id"], name: "index_identities_on_user_id"
+
   create_table "subchapters", force: :cascade do |t|
     t.string   "title"
     t.integer  "chapter_id"
@@ -60,12 +70,31 @@ ActiveRecord::Schema.define(version: 20151225141426) do
   add_index "subchapters", ["course_id"], name: "index_subchapters_on_course_id"
 
   create_table "users", force: :cascade do |t|
-    t.string   "provider"
-    t.string   "uid"
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
     t.string   "name"
-    t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
+    t.string   "provider"
+    t.string   "access_token"
+    t.string   "uid"
+    t.string   "twitter_secret"
   end
+
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
