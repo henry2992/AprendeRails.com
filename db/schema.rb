2d605f8af3612f9f2704009a3e9339f37afccb82
Irvin Loza <irvinloza@gmail.com>
2016-01-26 16:47:14 -0600

    Generated posts scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160104181925) do
+ActiveRecord::Schema.define(version: 20160126224001) do
 
   create_table "chapters", force: :cascade do |t|
     t.string   "title"
@@ -75,6 +75,13 @@ ActiveRecord::Schema.define(version: 20160104181925) do
   end
 
   add_index "identities", ["user_id"], name: "index_identities_on_user_id"
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "subchapters", force: :cascade do |t|
     t.string   "title"
